931f6c81320c791c5fa1db807438b2b6d3a27c8c
Vladyslav Depeshko <vdepeshko@e1r1p10.unit.ua>
2020-08-18 15:47:24 +0300

    refactoring made

--- t06/main/t06.c
+++ t06/main/t06.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -8,19 +7,37 @@
 #include <string.h>
 #include "driver/dac.h"
 #include "driver/ledc.h"
+#include "esp_log.h"
 
 #define GPIO_NUM_5   5
 #define GPIO_NUM_25  25
 
+void gpio_set_direction_wrapper(int gpio, int mode) {
+  if (gpio_set_direction(gpio, mode) != ESP_OK) {
+    ESP_LOGI("gpio_set_direction: ", "%s", "some error occured.");
+    exit(1);
+  }
+}
+
+
+
+void gpio_set_level_wrapper(int gpio, int level) {
+  if (gpio_set_level(gpio, level) != ESP_OK)
+    ESP_LOGI("gpio_set_level ", "%s", "some error occured.");
+}
+
+
+
 void app_main() {
-    dac_output_enable(DAC_CHANNEL_1);
-    gpio_set_direction(GPIO_NUM_5, GPIO_MODE_INPUT);
-    gpio_set_level(GPIO_NUM_5, 1);
+    if (dac_output_enable(DAC_CHANNEL_1) != ESP_OK) ESP_LOGI("dac_output_enable ", "%s", "some error occured.");
+    gpio_set_direction_wrapper(GPIO_NUM_5, GPIO_MODE_INPUT);
+    gpio_set_level_wrapper(GPIO_NUM_5, 1);
 
      while(1) {
         int v;
         for(v = 0; v < 255; v+= 30) {
-          dac_output_voltage(DAC_CHANNEL_1, v);
+          if (dac_output_voltage(DAC_CHANNEL_1, v) != ESP_OK) 
+            ESP_LOGI("dac_output_voltage ", "%s", "some error occured.");
           ets_delay_us(5000);
         }
     }
