8015d7c617c3963fa9b84477862916916b44466f
Vladyslav Depeshko <vdepeshko@e1r1p1.unit.ua>
2020-08-19 14:29:30 +0300

    t06 trash deleted

--- t06/main/t06.c
+++ t06/main/t06.c
@@ -8,25 +8,10 @@
 #include "driver/dac.h"
 #include "driver/ledc.h"
 #include "esp_log.h"
+#include "wrappers.h"
 
 #define GPIO_NUM_5   5
 #define GPIO_NUM_25  25
-
-void gpio_set_direction_wrapper(int gpio, int mode) {
-  if (gpio_set_direction(gpio, mode) != ESP_OK) {
-    ESP_LOGI("gpio_set_direction: ", "%s", "some error occured.");
-    exit(1);
-  }
-}
-
-
-
-void gpio_set_level_wrapper(int gpio, int level) {
-  if (gpio_set_level(gpio, level) != ESP_OK)
-    ESP_LOGI("gpio_set_level ", "%s", "some error occured.");
-}
-
-
 
 void app_main() {
     if (dac_output_enable(DAC_CHANNEL_1) != ESP_OK) ESP_LOGI("dac_output_enable ", "%s", "some error occured.");
