ae40a9005d876a14381b6141f40babd1305242f8
Vladyslav Depeshko <vdepeshko@e2r6p6.unit.ua>
2020-08-25 18:58:07 +0300

    t08

---
+++ t08/main/wrappers.c
@@ -0,0 +1,19 @@
+#include "wrappers.h"
+
+
+
+void gpio_set_direction_wrapper(int gpio, int mode) {
+	if (gpio_set_direction(gpio, mode) != ESP_OK) {
+		ESP_LOGI("gpio_set_direction: ", "%s", "some error occured.");
+		exit(1);
+	}
+}
+
+
+
+void gpio_set_level_wrapper(int gpio, int level) {
+	if (gpio_set_level(gpio, level) != ESP_OK) {
+		ESP_LOGI("gpio_set_level ", "%s", "some error occured.");
+		exit(1);
+	}
+}
