673b8f91585c77636dfb078e6b384edf6be0e073
Vladyslav Depeshko <vdepeshko@e2r4p9.unit.ua>
2020-08-28 16:52:20 +0300

    condition fixed

--- t08/main/t08.c
+++ t08/main/t08.c
@@ -10,10 +10,14 @@
 #include "driver/spi_master.h"
 #include "driver/gpio.h"
 #include "wrappers.h"
+#include "driver/dac.h"
 
 #define GPIO_LED1 27
 #define GPIO_LED2 26
 #define GPIO_LED3 33
+
+#define GPIO_NUM_5   5
+#define GPIO_NUM_25  25
 
 // Power for accelerometr.
 #define GPIO_EN_ACCEL 23
@@ -30,17 +34,13 @@
 /** @brief ADXL345 register read flag. */
 #define ADXL345_REG_READ_FLAG  0x80u
 /** @brief ADXL345 register multibyte flag. */
-#define ADXL345_REG_MB_FLAG  0x40u
+#define ADXL345_REG_MB_FLAG    0x40u
 /** @brief ADXL345 register: DATAX0. */
-#define ADXL345_REG_DATAX0  0x32u
-
-/** @brief ADXL345 register: DEVID. */
-#define ADXL345_REG_DEVID  0x00u
+#define ADXL345_REG_DATAX0     0x32u
 /** @brief ADXL345 register: BW_RATE. */
-#define ADXL345_REG_BW_RATE  0x2Cu
+#define ADXL345_REG_BW_RATE    0x2Cu
 /** @brief ADXL345 register: POWER_CTL. */
 #define ADXL345_REG_POWER_CTL  0x2Du
-
 
 /** @brief ADXL345 POWER_CTL flag: Measure. */
 #define ADXL345_POWER_CTL_MEASURE  0x08u
@@ -109,7 +109,7 @@ static void adxl345_write(spi_device_handle_t spi, uint8_t address, uint8_t valu
  *   - `[1]`: y-acceleration
  *   - `[2]`: z-acceleration
  */
-static void adxl345_read_acceleration (spi_device_handle_t spi, int16_t* accs) {
+static void adxl345_read_acceleration(spi_device_handle_t spi, int16_t* accs) {
     esp_err_t ret;
     uint8_t tx_buffer[3u * sizeof(uint16_t)]; // a dummy buffer
     spi_transaction_t trans = {
@@ -137,9 +137,26 @@ static void adxl345_read_acceleration (spi_device_handle_t spi, int16_t* accs) {
  *
  *   Handle of the ADXL345 to start.
  */
-static void adxl345_start (spi_device_handle_t spi) {
+static void adxl345_start(spi_device_handle_t spi) {
     adxl345_write(spi, ADXL345_REG_POWER_CTL, ADXL345_POWER_CTL_MEASURE);
     vTaskDelay(ADXL345_UPDATE_DELAY);
+}
+
+static void do_sound() {
+    int counter = 0;
+    while(counter != 1000) {
+        int v;
+        for(v = 0; v < 255; v+= 30) {
+        if (dac_output_voltage(DAC_CHANNEL_1, v) != ESP_OK) 
+            ESP_LOGI("dac_output_voltage ", "%s", "some error occured.");
+        }
+        for(v = 255; v > 0; v-= 30) {
+            if (dac_output_voltage(DAC_CHANNEL_1, v) != ESP_OK) 
+                ESP_LOGI("dac_output_voltage ", "%s", "some error occured.");
+        }
+        ets_delay_us(1);
+        counter +=1;
+    }
 }
 
 
@@ -151,20 +168,26 @@ static void adxl345_start (spi_device_handle_t spi) {
  *   (`spi_device_handle_t`) Handle to the ADXL345 from which the latest
  *   accleration is to be read.
  */
-static void adxl345_read_acceleration_task (void* pvParameters) {
+static void accelerator(void* pvParameters) {
     int16_t accs[3];
+    int x;
+    int y;
+    int z;
+
     spi_device_handle_t spi = (spi_device_handle_t)pvParameters;
     while (1) {
         adxl345_read_acceleration(spi, accs);
-        printf(
-            "ax, ay, az: %d, %d, %d\n",
-            (int)accs[0],
-            (int)accs[1],
-            (int)accs[2]);
- 		if (x >= 246 || x <= -263 || y >= 254 || y <= -250) {
+
+        x = (int)accs[0];
+        y = (int)accs[1];
+        z = (int)accs[2];
+
+        printf("%d %d %d\n", x,y,z);
+ 		if ((x >= 246 && x <= 300)|| (x <= -263 && x >= -300)|| (y >= 254 && y <= 300) || (y <= -250 && y >= -300) ) {
 	 		gpio_set_level_wrapper(GPIO_LED1, 1);
 	 		gpio_set_level_wrapper(GPIO_LED2, 1);
 	 		gpio_set_level_wrapper(GPIO_LED3, 1);
+            do_sound();
  		}
  		else {
  			gpio_set_level_wrapper(GPIO_LED1, 0);
@@ -176,17 +199,9 @@ static void adxl345_read_acceleration_task (void* pvParameters) {
     }
 }
 
-void app_main (void) {
-	gpio_set_direction_wrapper(GPIO_LED1, GPIO_MODE_OUTPUT);
-	gpio_set_direction_wrapper(GPIO_LED2, GPIO_MODE_OUTPUT);
-	gpio_set_direction_wrapper(GPIO_LED3, GPIO_MODE_OUTPUT);
 
-	gpio_set_direction_wrapper(23, GPIO_MODE_OUTPUT);
-	gpio_set_level_wrapper(23, 1);
 
-    esp_err_t ret;
-    spi_device_handle_t spi;
-
+spi_bus_config_t bus_config_init() {
     spi_bus_config_t bus_config = {
         .miso_io_num        = PIN_NUM_MISO,
         .mosi_io_num        = PIN_NUM_MOSI,
@@ -194,24 +209,51 @@ void app_main (void) {
         .quadwp_io_num      = -1,
         .quadhd_io_num      = -1
     };
+    return bus_config;
+}
 
+
+
+spi_device_interface_config_t device_config_init() {
     spi_device_interface_config_t device_config = {
-        .clock_speed_hz = 1000000, 			// 1Mbps. nearest clock will be chosen.
-        .mode 			= 3,                // CPOL=1, CPHA=1
+        .clock_speed_hz = 1000000,          // 1Mbps. nearest clock will be chosen.
+        .mode           = 3,                // CPOL=1, CPHA=1
         .spics_io_num   = PIN_NUM_CS,
         .command_bits   = 8,                // ADXL345 always takes 1+7 bit command (address).
         .queue_size     = 1 
     };
 
+    return device_config;
+}
+
+
+
+void app_main (void) {
+    spi_device_handle_t spi;
+
+    if (dac_output_enable(DAC_CHANNEL_1) != ESP_OK) ESP_LOGI("dac_output_enable ", "%s", "some error occured.");
+    gpio_set_direction_wrapper(GPIO_NUM_5, GPIO_MODE_INPUT);
+    gpio_set_level_wrapper(GPIO_NUM_5, 1);
+    // Enables led.
+	gpio_set_direction_wrapper(GPIO_LED1, GPIO_MODE_OUTPUT);
+	gpio_set_direction_wrapper(GPIO_LED2, GPIO_MODE_OUTPUT);
+	gpio_set_direction_wrapper(GPIO_LED3, GPIO_MODE_OUTPUT);
+    // Enable accelerometer.
+	gpio_set_direction_wrapper(GPIO_EN_ACCEL, GPIO_MODE_OUTPUT);
+	gpio_set_level_wrapper(GPIO_EN_ACCEL, 1);
+
+    spi_bus_config_t bus_config                 = bus_config_init();
+    spi_device_interface_config_t device_config = device_config_init();
+
     // initializes the SPI bus
-    ret = spi_bus_initialize(VSPI_HOST, &bus_config, DMA_CHAN);
-    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(spi_bus_initialize(VSPI_HOST, &bus_config, DMA_CHAN));
     // attaches the ADXL to the SPI bus
-    ret = spi_bus_add_device(VSPI_HOST, &device_config, &spi);
-    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(spi_bus_add_device(VSPI_HOST, &device_config, &spi));
 
     // starts sampling
     adxl345_start(spi);
-    
-    xTaskCreate(adxl345_read_acceleration_task, "acceleration", 2048u, (void*)spi, 5, 0);
+    xTaskCreate(accelerator, "acceleration", 2048u, (void*)spi, 5, 0);
 }
+
+
+
