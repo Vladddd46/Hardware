bad13efdcc8c3ebff0b71e93098d4d8add8b5972
Vladyslav <Vladddd46>
2020-08-31 20:06:01 +0300

    add comments in creative

--- creative/main/creative.c
+++ creative/main/creative.c
@@ -14,25 +14,31 @@
 
 #define GPIO_BUTTON1 39
 #define GPIO_BUTTON2 18
-#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_LED1) | (1ULL<<GPIO_LED2))
-#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_BUTTON1) | (1ULL<<GPIO_BUTTON2))
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_LED1)    | (1ULL<<GPIO_LED2))
+#define GPIO_INPUT_PIN_SEL   ((1ULL<<GPIO_BUTTON1) | (1ULL<<GPIO_BUTTON2))
 #define ESP_INTR_FLAG_DEFAULT 0
 
 #define OLED_ENABLE 32
 
 static xQueueHandle gpio_evt_queue = NULL;
-pthread_mutex_t mutex;
+static pthread_mutex_t mutex;
+static int screen_num  = 0;
+static int temperature = 0;
+static int humidity    = 0;
 
+
+/*
+ * ISR, which handles button press.
+ * Sets global variable "screen_num" to
+ * the corresponding screen.
+ */
 static void IRAM_ATTR gpio_isr_handler(void *arg) {
     uint32_t gpio_num = (uint32_t)arg;
 
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
 
-
-
-static int screen_num = 0;
-static void switch_handler(void* arg) {
+static void switch_button_handler(void* arg) {
     uint32_t io_num;
 
     for(;;) {
@@ -51,9 +57,14 @@ static void switch_handler(void* arg) {
     }
 }
 
-static int temperature = 0;
-static int humidity    = 0;
 
+
+/*
+ * Gets data from dht11 depending on 
+ * the current screen state.
+ * Places result in 
+ * global vars. temperature/humidity. 
+ */
 static void dht11_data_checker() {
     int res;
 
@@ -72,8 +83,10 @@ static void dht11_data_checker() {
 
 
 
-// Initialize temperature and humidity value;
-static void dht11_init() {
+/*
+ * Initialize temperature and humidity values.
+ */
+static void dht11_data_init() {
     int counter = 0;
     int res;
 
@@ -97,48 +110,41 @@ static void dht11_init() {
 }
 
 
-static void checker(void *s) {
+
+/*
+ * Draws screen page depending on screen_num.
+ */
+static void drawer(void) {
     sh1106_t display;
     sh1106_init(&display);
     sh1106_clear(&display);
-
     sh1106_t *display1 = &display;
 
+    char res[50];
     while(1) {
         pthread_mutex_lock(&mutex);
-        if (screen_num == 0) {
-            sh1106_clear(&display);
-
-            char res[50];
-            bzero(res, 50);
+        bzero(res, 50);
+        sh1106_clear(&display);
+        if (screen_num == 0)
             sprintf(res, "    humidity: %d %%", humidity);
-
-            print_str_in_line(&display1, res, 3);
-        }
-        else if (screen_num == 1) {
-            sh1106_clear(&display);
-
-            char res[50];
-            bzero(res, 50);
+        else if (screen_num == 1)
             sprintf(res, "   temperature: %d C", temperature);
 
-            print_str_in_line(&display1, res, 3);
-        }
+        print_str_in_line(&display1, res, 3);
         sh1106_update(&display);
+
         pthread_mutex_unlock(&mutex);
-        vTaskDelay(100);
+        vTaskDelay(10);
     }
 }
 
 
-void app_main(void) {
 
-    // Oled enable
-    gpio_set_direction_wrapper(OLED_ENABLE, GPIO_MODE_OUTPUT);
-    gpio_set_level_wrapper(OLED_ENABLE, 1);
-
-    init_i2c_driver();
-
+/*
+ * Darws waiting page, while dht11 needs
+ * to process data.
+ */
+static void wait_page_draw() {
     sh1106_t display;
     sh1106_init(&display);
     sh1106_clear(&display);
@@ -146,7 +152,18 @@ void app_main(void) {
     sh1106_t *display1 = &display;
     print_str_in_line(&display1, "        Wait...", 3);
     sh1106_update(&display);
+}
 
+
+
+void app_main(void) {
+    // Oled enable
+    gpio_set_direction_wrapper(OLED_ENABLE, GPIO_MODE_OUTPUT);
+    gpio_set_level_wrapper(OLED_ENABLE, 1);
+
+    init_i2c_driver();
+    wait_page_draw();
+    dht11_data_init();
 
     gpio_config_t io_conf;
     io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
@@ -161,9 +178,8 @@ void app_main(void) {
 
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
 
-    dht11_init();
-    xTaskCreate(switch_handler, "switch_handler", 4048, NULL, 10, NULL);
-    xTaskCreate(checker, "checker", 4048, NULL, 10, NULL);
+    xTaskCreate(switch_button_handler, "switch_button_handler", 4048, NULL, 10, NULL);
+    xTaskCreate(drawer, "drawer", 4048, NULL, 10, NULL);
     xTaskCreate(dht11_data_checker, "dht11_data_checker", 4048, NULL, 10, NULL);
 
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
