4a09bd37e9dc50d640d57ab491857a6eb1b2b735
Vladyslav <Vladddd46>
2020-09-02 18:24:29 +0300

    t08 improved

--- t08/main/t08.c
+++ t08/main/t08.c
@@ -1,4 +1,4 @@
-/*
+ /*
  * ADXL345 control via SPI.
  */
 #include <stdio.h>
@@ -12,17 +12,13 @@
 #include "wrappers.h"
 #include "driver/dac.h"
 
-#define GPIO_LED1 27
-#define GPIO_LED2 26
-#define GPIO_LED3 33
-
-#define GPIO_NUM_5   5
-#define GPIO_NUM_25  25
-
-// Power for accelerometr.
+#define GPIO_LED1     27
+#define GPIO_LED2     26
+#define GPIO_LED3     33
+#define GPIO_NUM_5    5
+#define GPIO_NUM_25   25
 #define GPIO_EN_ACCEL 23
 
-// @brief DMA channel is not used.
 #define DMA_CHAN  0
 
 #define PIN_NUM_MISO  12
@@ -30,55 +26,16 @@
 #define PIN_NUM_CLK   14
 #define PIN_NUM_CS    15
 
-
-/** @brief ADXL345 register read flag. */
 #define ADXL345_REG_READ_FLAG  0x80u
-/** @brief ADXL345 register multibyte flag. */
 #define ADXL345_REG_MB_FLAG    0x40u
-/** @brief ADXL345 register: DATAX0. */
 #define ADXL345_REG_DATAX0     0x32u
-/** @brief ADXL345 register: BW_RATE. */
 #define ADXL345_REG_BW_RATE    0x2Cu
-/** @brief ADXL345 register: POWER_CTL. */
 #define ADXL345_REG_POWER_CTL  0x2Du
-
-/** @brief ADXL345 POWER_CTL flag: Measure. */
 #define ADXL345_POWER_CTL_MEASURE  0x08u
-
-/** @brief ADXL345 delay to update (200ms). */
 #define ADXL345_UPDATE_DELAY  (200u / portTICK_PERIOD_MS)
 
 
 
-/**
- * @brief Writes a given value in a specified register of an ADXL345.
- *
- * In a 4-wire SPI transaction, transmission and receiving happen in parallel
- * as shown below.
- *
- * ```
- * Transmit: address --> t.tx_data[0]
- * Receive:  N/A     --> t.rx_data[0]
- *
- * where t is spi_transaction_t and
- * t.tx_data[0] = value
- * t.rx_data[0] = do not care
- * ```
- *
- * This function blocks until the transmission and receiving end.
- *
- * @param[in] spi
- *
- *   Handle of the ADXL345 where the register value is to be set.
- *
- * @param[in] address
- *
- *   Address of the register to be written.
- *
- * @param[in] value
- *
- *   Value to be written to the register associated with `address`.
- */
 static void adxl345_write(spi_device_handle_t spi, uint8_t address, uint8_t value) {
     esp_err_t ret;
 
@@ -92,55 +49,31 @@ static void adxl345_write(spi_device_handle_t spi, uint8_t address, uint8_t valu
     assert(ret == ESP_OK);
 }
 
-/**
- * @brief Reads the latest acceleration from the ADXL345.
- *
- * Undefined if `accs` does not point to a block smaller than
- * `sizeof(int16_t) * 3`
- *
- * @param[in] spi
- *
- *   Handle of the ADXL345 from which the latest acceleration is to be read.
- *
- * @param[out] accs
- *
- *   Buffer to receive acceleration.
- *   - `[0]`: x-acceleration
- *   - `[1]`: y-acceleration
- *   - `[2]`: z-acceleration
- */
+
+
 static void adxl345_read_acceleration(spi_device_handle_t spi, int16_t* accs) {
     esp_err_t ret;
-    uint8_t tx_buffer[3u * sizeof(uint16_t)]; // a dummy buffer
+    uint8_t tx_buffer[3u * sizeof(uint16_t)];
     spi_transaction_t trans = {
         .cmd = ADXL345_REG_READ_FLAG |
             ADXL345_REG_MB_FLAG |
             ADXL345_REG_DATAX0,
-        .length = sizeof(tx_buffer) * 8, // in bits
+        .length = sizeof(tx_buffer) * 8,
         .tx_buffer = tx_buffer,
         .rx_buffer = accs
     };
     ret = spi_device_polling_transmit(spi, &trans);
     assert(ret == ESP_OK);
-    // sample of each axis is represented in twos complement.
-    // and as ESP32 is little endian, `accs` does not need swapping.
-    // https://docs.espressif.com/projects/esp-idf/en/latest/api-reference/peripherals/spi_master.html#transactions-with-integers-other-than-uint8-t
 }
 
 
-/**
- * @brief Starts sampling of the ADXL345.
- *
- * This function blocks for 200ms after sending a start command.
- *
- * @param[in] spi
- *
- *   Handle of the ADXL345 to start.
- */
+
 static void adxl345_start(spi_device_handle_t spi) {
     adxl345_write(spi, ADXL345_REG_POWER_CTL, ADXL345_POWER_CTL_MEASURE);
     vTaskDelay(ADXL345_UPDATE_DELAY);
 }
+
+
 
 static void do_sound() {
     int counter = 0;
@@ -160,14 +93,7 @@ static void do_sound() {
 }
 
 
-/**
- * @brief Task that periodically reads accelerations.
- *
- * @param[in] pvParameters
- *
- *   (`spi_device_handle_t`) Handle to the ADXL345 from which the latest
- *   accleration is to be read.
- */
+
 static void accelerator(void* pvParameters) {
     int16_t accs[3];
     int x;
@@ -183,7 +109,8 @@ static void accelerator(void* pvParameters) {
         z = (int)accs[2];
 
         printf("%d %d %d\n", x,y,z);
- 		if ((x >= 246 && x <= 300)|| (x <= -263 && x >= -300)|| (y >= 254 && y <= 300) || (y <= -250 && y >= -300) ) {
+ 		if (   (x >= 246 && x <= 300) || (x <= -263 && x >= -300)
+            || (y >= 254 && y <= 300) || (y <= -250 && y >= -300) ) {
 	 		gpio_set_level_wrapper(GPIO_LED1, 1);
 	 		gpio_set_level_wrapper(GPIO_LED2, 1);
 	 		gpio_set_level_wrapper(GPIO_LED3, 1);
